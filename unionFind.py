56035b1002ce0ea90ff7b2287e9c52bc7741b11f
Valink16 <valuthyk@gmail.com>
2020-01-12 13:15:47 +0100

    Initial implementation of Union-Find

---
+++ unionFind.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.parent = None
+    
+    def __repr__(self):
+        if self.parent == None:
+            added = " Root"
+        else:
+            added = " " + str(self.parent.id)
+        return str(self.id) + added
+
+    def find(self):
+        if self.parent == None:
+            return self
+        else:
+            root = self.parent.find()
+            self.parent = root
+            return root
+
+            #return self.parent.find()
+
+    def unite(self, other):
+        p1, p2 = self.find(), other.find()
+        if not p1 == p2:
+            p1.parent = p2
